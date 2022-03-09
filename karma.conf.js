2d82ef006a6159e0e3bfb07f2f468cf65e3fd282
Alex Maiburg <alex@maiburg.com>
2022-03-09 21:06:03 +0100

    exclude karma.conf.js from .gitignore

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function (config) {
+  const options = {
+    basePath: 'src',
+    frameworks: ['jasmine'],
+    reporters: ['mocha'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    customLaunchers: {
+      android: {
+        base: 'NS',
+        platform: 'android'
+      },
+      ios: {
+        base: 'NS',
+        platform: 'ios'
+      },
+      ios_simulator: {
+        base: 'NS',
+        platform: 'ios',
+        arguments: ['--emulator']
+      }
+    },
+    singleRun: false
+  };
+
+  if (config._NS && config._NS.env && config._NS.env.codeCoverage) {
+    options.reporters = (options.reporters || []).concat(['coverage']);
+  }
+
+  config.set(options);
+};
