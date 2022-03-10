ad8171ef53b07ec18090186e30ce2a184fb51326
Alex Maiburg <alex@maiburg.com>
2022-03-10 14:42:21 +0100

    test if ci pipeline interrupts

--- karma.conf.js
+++ karma.conf.js
@@ -30,6 +30,8 @@ module.exports = function (config) {
     singleRun: process.env.USER === 'runner'
   };
 
+  console.log('RRRRRRRRRR', config.singleRun);
+
   if (config._NS && config._NS.env && config._NS.env.codeCoverage) {
     options.reporters = (options.reporters || []).concat(['coverage']);
   }
