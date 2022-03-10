f378beca4f45803974a9eec623cf8f485b320975
Alex Maiburg <alex@maiburg.com>
2022-03-10 14:41:23 +0100

    test if ci pipeline interrupts

--- src/app/timer/timer-list/timer-list.component.spec.ts
+++ src/app/timer/timer-list/timer-list.component.spec.ts
@@ -19,7 +19,7 @@ describe('TimerListComponent', () => {
 
   describe('returnTwo() should', () => {
     it('return 2', () => {
-      const num = 2;
+      const num = 3;
 
       expect(component.returnTwo).toBe(num);
     });
