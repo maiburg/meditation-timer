728516ba62de591b251550dc64e18e6de5dff580
Alex Maiburg <alex@maiburg.com>
2022-07-01 14:56:54 +0200

    access page and device

--- src/app/features/timer/components/timer-presettings/timer-presettings.component.ts
+++ src/app/features/timer/components/timer-presettings/timer-presettings.component.ts
@@ -3,17 +3,23 @@ import { ActivatedRoute } from '@angular/router';
 
 import { TimerService } from '@app/features/timer/services';
 import { TimerPresetting } from '@core/models/domain';
+import { Device, Page } from '@nativescript/core';
 
 @Component({
   selector: 'app-timer-presettings',
   templateUrl: './timer-presettings.component.html',
-  styleUrls: ['./timer-presettings.component.scss'],
-  moduleId: module.id
+  styleUrls: ['./timer-presettings.component.scss']
 })
 export class TimerPresettingsComponent implements OnInit {
+  output = '';
   presettings: TimerPresetting[];
 
-  constructor(private route: ActivatedRoute, private timerService: TimerService) {}
+  constructor(private route: ActivatedRoute, private timerService: TimerService, private page: Page) {
+    page.backgroundColor = '#ff0000';
+    page.backgroundSpanUnderStatusBar = true;
+    // page.actionBarHidden = true;
+    this.output = Device.osVersion;
+  }
 
   ngOnInit(): void {
     this.presettings = this.route.snapshot.data['presettings'];
