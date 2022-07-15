b086e7b21141fef9a2276bd62a3d7a0b43243bbd
Alex Maiburg <alex@maiburg.com>
2022-07-15 11:55:47 +0200

    complete unit tests for backlog.repository

--- src/app/features/backlog/repositories/backlog.repository.spec.ts
+++ src/app/features/backlog/repositories/backlog.repository.spec.ts
@@ -1,22 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { BacklogRepository } from '@features/backlog/repositories';
 import { APP_CONFIG } from '@src/config/app-config.module';
 
 describe('BacklogRepository', () => {
-  let service: BacklogRepository;
+  let service: BacklogRepository, http: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [BacklogRepository, { provide: APP_CONFIG, useValue: {} }, HttpClient]
+      providers: [BacklogRepository, HttpClient, { provide: APP_CONFIG, useValue: {} }]
     });
     service = TestBed.inject(BacklogRepository);
+    http = TestBed.inject(HttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('backlogUrl() should', () => {
+    it('call http.get()', () => {
+      const spy = spyOn(http, 'get').and.returnValue(of({}));
+      const url = 'foo/bar/baz';
+      spyOnProperty<any>(service, 'backlogUrl', 'get').and.returnValue(url);
+
+      service.getPtItems(null, null);
+
+      expect(spy).toHaveBeenCalledWith(url);
+    });
   });
 });
