1b45275a355fda1732162d7bad3383d6b110b5dc
Alex Maiburg <alex@maiburg.com>
2022-07-15 13:46:45 +0200

    remove backlog code from app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { AppComponent } from '@app/app.component';
-import { LoggerService, SqliteService } from '@app/core/services';
+import { SqliteService } from '@app/core/services';
 import { TranslateLoaderStub } from '@app/utils';
-import { ServerErrorHandlerService, StoreService } from '@core/services';
-import { BacklogRepository } from '@features/backlog/repositories';
-import { BacklogService } from '@features/backlog/services';
 import { AppConfigModule } from '@src/config/app-config.module';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let sqlite: SqliteService, backlogService: BacklogService;
+  let sqlite: SqliteService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,37 +24,19 @@ describe('AppComponent', () => {
         })
       ],
       declarations: [AppComponent],
-      providers: [
-        SqliteService,
-        TranslateService,
-        LoggerService,
-        StoreService,
-        BacklogService,
-        BacklogRepository,
-        HttpClient,
-        ServerErrorHandlerService
-      ]
+      providers: [SqliteService, TranslateService]
     }).compileComponents();
   });
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     sqlite = TestBed.inject(SqliteService);
-    backlogService = TestBed.inject(BacklogService);
     fixture.detectChanges();
   });
 
   describe('ngOnInit() should', () => {
     it('call sqlite.initDB()', () => {
       const spy = spyOn(sqlite, 'initDB');
-
-      component.ngOnInit();
-
-      expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it('call backlogService.fetchItems()', () => {
-      const spy = spyOn(backlogService, 'fetchItems');
 
       component.ngOnInit();
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SqliteService, StoreService } from '@app/core/services';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
 
-import { State } from '@core/models/core';
-import { PtItem } from '@core/models/domain';
-import { BacklogService } from '@features/backlog/services';
+import { SqliteService } from '@app/core/services';
 
 console.log('AppComponent loaded');
 
@@ -15,20 +11,12 @@ console.log('AppComponent loaded');
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  items$: Observable<PtItem[]> = this.store.select((state: State) => state.backlogItems);
-
-  constructor(
-    private readonly sqlite: SqliteService,
-    private readonly translate: TranslateService,
-    private readonly store: StoreService,
-    private readonly backlogService: BacklogService
-  ) {
+  constructor(private readonly sqlite: SqliteService, private readonly translate: TranslateService) {
     translate.setDefaultLang('de');
     translate.use('de');
   }
 
   ngOnInit(): void {
-    this.backlogService.fetchItems();
     this.sqlite.initDB();
   }
 }
